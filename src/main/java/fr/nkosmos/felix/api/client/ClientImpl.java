894a61d25b3713c919279599da069794e1fba6a9
xTrM_ <xtrmyt24@gmail.com>
2021-02-02 15:57:18 +0100

    stonks

--- src/main/java/fr/nkosmos/felix/api/client/ClientImpl.java
+++ src/main/java/fr/nkosmos/felix/api/client/ClientImpl.java
@@ -26,110 +26,110 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = false)
 public @Data class ClientImpl extends Reliqua implements FelixClient {
 
-	private static final Logger LOGGER = LogManager.getLogger("FelixClient");
+    private static final Logger LOGGER = LogManager.getLogger("FelixClient");
     private static final String API_BASE = "https://nkosmos.fr/api/v2";
     
-	private final String userAgent;
-	private String apiKey, authToken;
+    private final String userAgent;
+    private String apiKey, authToken;
 
-	public ClientImpl(OkHttpClient client, RateLimiterFactory factory, boolean trackCallSites, String userAgent) {
+    public ClientImpl(OkHttpClient client, RateLimiterFactory factory, boolean trackCallSites, String userAgent) {
         super(client, factory, trackCallSites);
         this.userAgent = userAgent;
     }
 
-	@Override
-	public PendingRequest<AuthenticationResponse> requestAuthentication(String identifier, String authentifier) {
-		return null;
-	}
+    @Override
+    public PendingRequest<AuthenticationResponse> requestAuthentication(String identifier, String authentifier) {
+        return null;
+    }
 
-	@Override
-	public PendingRequest<User> requestUser(UUID userId) {
-		requiresApiKey();
-		return null;
-	}
+    @Override
+    public PendingRequest<User> requestUser(UUID userId) {
+        requiresApiKey();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<User.SelfUser> requestSelfUser() {
-		requiresToken();
-		return null;
-	}
+    @Override
+    public PendingRequest<User.SelfUser> requestSelfUser() {
+        requiresToken();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Resource> requestResource(UUID uuid) {
-		requiresApiKey();
-		return createRequest(
-					newRequestBuilder(Route.Defaults.Marketplace.GET_RESOURCE.compile(uuid.toString()).getURL(API_BASE))
-					.header("Authorization", "API " + apiKey)
-					.get()
-				)
+    @Override
+    public PendingRequest<Resource> requestResource(UUID uuid) {
+        requiresApiKey();
+        return createRequest(
+                    newRequestBuilder(Route.Defaults.Marketplace.GET_RESOURCE.compile(uuid.toString()).getURL(API_BASE))
+                    .header("Authorization", "API " + apiKey)
+                    .get()
+                )
                 .setRateLimiter(getRateLimiter("/marketplace"))
                 .setStatusCodeValidator(StatusCodeValidator.ACCEPT_200)
                 .build(response -> RequestUtils.toJson(response, Resource.class), RequestUtils::handleError);
-	}
+    }
 
-	@Override
-	public PendingRequest<Set<Resource>> requestResources(UUID userId) {
-		requiresApiKey();
-		return null;
-	}
+    @Override
+    public PendingRequest<Set<Resource>> requestResources(UUID userId) {
+        requiresApiKey();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Resource.PersonalResource> requestPersonalResource(UUID uuid) {
-		requiresToken();
-		return null;
-	}
+    @Override
+    public PendingRequest<Resource.PersonalResource> requestPersonalResource(UUID uuid) {
+        requiresToken();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Set<Resource.PersonalResource>> requestPersonalResources() {
-		requiresToken();
-		return null;
-	}
+    @Override
+    public PendingRequest<Set<Resource.PersonalResource>> requestPersonalResources() {
+        requiresToken();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Comment> requestComment(UUID uuid) {
-		requiresApiKey();
-		return null;
-	}
+    @Override
+    public PendingRequest<Comment> requestComment(UUID uuid) {
+        requiresApiKey();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Set<Comment>> requestComments(UUID userId) {
-		requiresApiKey();
-		return null;
-	}
+    @Override
+    public PendingRequest<Set<Comment>> requestComments(UUID userId) {
+        requiresApiKey();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Comment.PersonalComment> requestPersonalComment(UUID uuid) {
-		requiresToken();
-		return null;
-	}
+    @Override
+    public PendingRequest<Comment.PersonalComment> requestPersonalComment(UUID uuid) {
+        requiresToken();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Set<Comment.PersonalComment>> requestPersonalComments() {
-		requiresToken();
-		return null;
-	}
+    @Override
+    public PendingRequest<Set<Comment.PersonalComment>> requestPersonalComments() {
+        requiresToken();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Set<Application>> requestApplications() {
-		requiresApiKey();
-		return null;
-	}
+    @Override
+    public PendingRequest<Set<Application>> requestApplications() {
+        requiresApiKey();
+        return null;
+    }
 
-	@Override
-	public PendingRequest<Application> requestApplication(UUID uuid) {
-		requiresApiKey();
-		return null;
-	}
-	
-	private void requiresApiKey() {
-		if(apiKey == null || apiKey.isEmpty()) throw new IllegalStateException("This endpoint requires an API key.");
-	}
-	
-	private void requiresToken() {
-		if(authToken == null || authToken.isEmpty()) throw new IllegalStateException("This endpoint requires an Authentication token.");
-	}
-	
-	@CheckReturnValue
+    @Override
+    public PendingRequest<Application> requestApplication(UUID uuid) {
+        requiresApiKey();
+        return null;
+    }
+    
+    private void requiresApiKey() {
+        if(apiKey == null || apiKey.isEmpty()) throw new IllegalStateException("This endpoint requires an API key.");
+    }
+    
+    private void requiresToken() {
+        if(authToken == null || authToken.isEmpty()) throw new IllegalStateException("This endpoint requires an Authentication token.");
+    }
+    
+    @CheckReturnValue
     @Nonnull
     public Request.Builder newRequestBuilder(@Nonnull String url) {
         return new Request.Builder()
